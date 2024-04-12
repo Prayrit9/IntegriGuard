9ef656f35070ef6129bcf2407b5410e1bd51ba1c
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-04-12 06:31:43 +0530

    button added

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { WagmiConfig, createConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from "connectkit";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    infuraId: import.meta.env.VITE_INFURA_KEY, // or infuraId
+    walletConnectProjectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
+
+    // Required
+    appName: "IntegriGuard",
+
+    // Optional
+    appDescription: "Work status of project",
+    appUrl: "https://family.co",
+    appIcon: "https://i.pinimg.com/736x/64/d6/0f/64d60f03aff21e923652c55a686cc421.jpg",
+
+    chains: [sepolia],
+  })
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig config={config}>
+      <ConnectKitProvider theme="retro" >
+        <App />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
