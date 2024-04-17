5dfa3c7e4b7ac4f8e68e080ddabddead489a9963
Prayrit <dprayrit@gmail.com>
2024-04-17 19:29:29 +0530

    minorchange

--- react/src/components/Teams/Team1.tsx
+++ react/src/components/Teams/Team1.tsx
@@ -2,8 +2,24 @@ import React from "react";
 
 function Team1() {
   return (
-    <div className="h-[80vh] flex flex-col justify-center items-center font-semibold text-7xl">
-      Team1
+    <div className="h-[80vh] bg-[#D8C4B6] ">
+      <h1 className="text-center font-bold text-[5vh] pt-[10vh]">TEAM1</h1>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:47am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:44am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:42am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:39am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:37am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:34am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:32am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:28am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:25am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:22am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:19am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:16am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:13am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:10am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:06am<br/>
+      0x4F1b0e81723bbf095a137a96E25F51535fAd1Cb3 updated website at 10:02am<br/>
     </div>
   );
 }
