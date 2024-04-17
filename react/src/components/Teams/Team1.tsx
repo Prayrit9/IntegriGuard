28b96e7b0c2b3ab5fcc332de60dfafdf6b19c5d3
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-04-18 00:59:57 +0530

    input

--- react/src/components/Teams/Team1.tsx
+++ react/src/components/Teams/Team1.tsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { TextInput } from '@mantine/core';
+import { TextInput } from "@mantine/core";
+import { useState } from "react";
 
 function Team1() {
+  const [team1Data, setTeam1Data] = useState("");
+  const handleInput = (event: any) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(name, value);
+    setTeam1Data(value);
+  };
   return (
     <div className="h-[80vh] bg-[#D8C4B6] ">
-      <h1 className="text-center font-bold text-[#213555] text-[5vh] pt-[10vh]">TEAM1</h1>
+      <h1 className="text-center font-bold text-[#213555] text-[5vh] pt-[10vh]">
+        TEAM1
+      </h1>
       <div className="text-center flex justify-center gap-[2vw]">
-        <input placeholder="Enter the changes you made" className="w-[45vw] h-[4vh] p-[2vw] rounded-lg text-[#213555] bg-[#F5EFE7]"></input>
-        <button className="p-[1vw] border-solid bg-[#213555] text-[#F5EFE7] rounded-lg">Submit</button>
+        <input
+          placeholder="Enter the changes you made"
+          className="w-[45vw] h-[4vh] p-[2vw] rounded-lg text-[#213555] bg-[#F5EFE7]"
+          value={team1Data}
+          name="team1"
+          id="team1"
+          onChange={handleInput}
+        ></input>
+        <button className="p-[1vw] border-solid bg-[#213555] text-[#F5EFE7] rounded-lg">
+          Submit
+        </button>
       </div>
     </div>
   );
--- react/src/components/Teams/Team4.tsx
+++ react/src/components/Teams/Team4.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 function Team4() {
+  const [team4Data, setTeam4Data] = useState("");
+  const handleInput = (event: any) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(name, value);
+    setTeam4Data(value);
+  };
   return (
     <div className="h-[80vh] bg-[#D8C4B6] ">
       <h1 className="text-center font-bold text-[#213555] text-[5vh] pt-[10vh]">TEAM4</h1>
       <div className="text-center flex justify-center gap-[2vw]">
-        <input placeholder="Enter the changes you made" className="w-[45vw] h-[4vh] p-[2vw] rounded-lg text-[#213555] bg-[#F5EFE7]"></input>
+        <input placeholder="Enter the changes you made" className="w-[45vw] h-[4vh] p-[2vw] rounded-lg text-[#213555] bg-[#F5EFE7]"
+         value={team4Data}
+         name="team4"
+         id="team4"
+         onChange={handleInput}></input>
         <button className="p-[1vw] border-solid bg-[#213555] text-[#F5EFE7] rounded-lg">Submit</button>
       </div>
     </div>
