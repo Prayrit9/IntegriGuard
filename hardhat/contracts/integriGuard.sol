860af04a7bdb1b2f3229d26f9df1deb64bcd9615
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-04-18 01:32:15 +0530

    write

--- hardhat/contracts/integriGuard.sol
+++ hardhat/contracts/integriGuard.sol
@@ -4,50 +4,52 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 contract integriGuard {
-
-        using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.AddressSet;
     using EnumerableSet for EnumerableSet.UintSet;
-        using EnumerableSet for EnumerableSet.Bytes32Set;
+    using EnumerableSet for EnumerableSet.Bytes32Set;
 
+    mapping(string => EnumerableSet.AddressSet) private addressSet;
+    mapping(string => EnumerableSet.UintSet) private timeSet;
+    mapping(string => EnumerableSet.Bytes32Set) private dataSet;
 
-    EnumerableSet.AddressSet private mySet;
-mapping (address=> EnumerableSet.AddressSet) private  addressSet;
-mapping (address=> EnumerableSet.UintSet) private  timeSet;
-mapping (address=> EnumerableSet.Bytes32Set) private  dataSet;
+    address[] senderAddress;
+    mapping(string => address[]) private address2Set;
 
-   
-   function setStatus(string memory data) public {
-     bytes memory tempEmptyStringTest = bytes(data);
-    if (tempEmptyStringTest.length == 0) {
-        return ;
+    function setStatus(string memory data, string memory team) public {
+        bytes memory tempEmptyStringTest = bytes(data);
+        if (tempEmptyStringTest.length == 0) {
+            return;
+        }
+
+        bytes32 result;
+        assembly {
+            result := mload(add(data, 32))
+        }
+        address2Set[team].push(msg.sender);
+        dataSet[team].add(result);
+        addressSet[team].add(msg.sender);
+        timeSet[team].add(block.timestamp);
     }
 
-    bytes32 result;
-    assembly {
-        result := mload(add(data, 32))
-    }
-   
-    dataSet[msg.sender].add(result);
-    addressSet[msg.sender].add(msg.sender);
-    timeSet[msg.sender].add(block.timestamp);
-   }
-
-
-
-  function getData(
-        
+    function getData(
+        string memory team
     ) public view returns (bytes32[] memory) {
-        return dataSet[msg.sender].values();
-    }
-     function getAddress(
-        
-    ) public view returns (address[] memory) {
-        return addressSet[msg.sender].values();
-    }
-     function getTime(
-        
-    ) public view returns (uint[] memory) {
-        return timeSet[msg.sender].values();
+        return dataSet[team].values();
     }
 
+    function getAddress(
+        string memory team
+    ) public view returns (address[] memory) {
+        return addressSet[team].values();
+    }
+
+    function getAddress2(
+        string memory team
+    ) public view returns (address[] memory) {
+        return address2Set[team];
+    }
+
+    function getTime(string memory team) public view returns (uint[] memory) {
+        return timeSet[team].values();
+    }
 }
--- react/src/components/Teams/Team1.tsx
+++ react/src/components/Teams/Team1.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { TextInput } from "@mantine/core";
 import { useState } from "react";
-
+import integriGuard from "../../../../hardhat/artifacts/contracts/integriGuard.sol/integriGuard.json";
+import smartContractAddress from "../../smartContractAddress.json";
+import { createWalletClient, custom, createPublicClient, http } from "viem";
+import { sepolia } from "viem/chains";
+import { useAccount } from "wagmi";
 function Team1() {
   const [team1Data, setTeam1Data] = useState("");
   const handleInput = (event: any) => {
@@ -10,6 +14,30 @@ function Team1() {
     console.log(name, value);
     setTeam1Data(value);
   };
+
+  const { address } = useAccount();
+
+  const publicClient = createPublicClient({
+    chain: sepolia,
+    transport: http(),
+  });
+
+  const walletClient = createWalletClient({
+    chain: sepolia,
+    transport: custom((window as any).ethereum),
+  });
+
+  const addTeam1Data = async () => {
+    const { request } = await publicClient.simulateContract({
+      address: smartContractAddress.smartContractAddress as `0x${string}`,
+      abi: integriGuard.abi,
+      functionName: "setStatus",
+      args: [team1Data, "team1"],
+      account: address,
+    });
+    await walletClient.writeContract(request);
+  };
+
   return (
     <div className="h-[80vh] bg-[#D8C4B6] ">
       <h1 className="text-center font-bold text-[#213555] text-[5vh] pt-[10vh]">
@@ -24,7 +52,10 @@ function Team1() {
           id="team1"
           onChange={handleInput}
         ></input>
-        <button className="p-[1vw] border-solid bg-[#213555] text-[#F5EFE7] rounded-lg">
+        <button
+          className="p-[1vw] border-solid bg-[#213555] text-[#F5EFE7] rounded-lg"
+          onClick={addTeam1Data}
+        >
           Submit
         </button>
       </div>
